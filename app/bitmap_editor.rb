26daeee7e2021a2256de82d7a5d3c2460360cf71
Akash <akash@users.noreply.github.com>
2016-03-22 14:04:03 +0000

    fixed help display text

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -30,7 +30,6 @@ C - Clears the table, setting all pixels to white (O).
 L X Y C - Colours the pixel (X,Y) with colour C.
 V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
 H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
-F X Y C - Fill the region R with the colour C. R is defined as: Pixel (X,Y) belongs to R. Any other pixel which is the same colour as (X,Y) and shares a common side with any pixel in R also belongs to this region.
 S - Show the contents of the current image
 X - Terminate the session"
     end
